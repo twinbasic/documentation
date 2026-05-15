6552361fcffe5fe50b11286d83062131be086788
Kuba Sunderland-Ober <kuba@mareimbrium.org>
2026-05-15 20:01:51 +0200

    Add a simple build script, use local stylesheet links.

---
+++ docs/book.bat
@@ -0,0 +1,4 @@
+@echo off
+bundle exec jekyll build --config _config.yml,_config-pdf.yml || exit /b
+if not exist _pdf mkdir _pdf
+npx pagedjs-cli _site-pdf\book.html -o _pdf\book.pdf --outline-tags h1,h2,h3 -t 180000
