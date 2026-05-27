19d98ea01a0e32f892c093e81e5333d9c4cae1a1
Kuba Sunderland-Ober <kuba@mareimbrium.org>
2026-05-28 01:07:21 +0200

    Add book PDF pass to check.bat to match CI

--- docs/check.bat
+++ docs/check.bat
@@ -1,12 +1,15 @@
 @pushd "%~dp0..\"
 node scripts/check_links.mjs --offline --include-fragments ^
     --check-html --check-a11y --check-ids ^
-    --check-sitemap --check-search ^
+    --check-sitemap --check-search --check-canonical ^
     --fallback-extensions html --index-files "index.html,." ^
     --root-dir docs/_site docs/_site /sep/ ^
     --offline --include-fragments ^
     --check-html --check-a11y --check-ids ^
     --forbid "https://docs.twinbasic.com" ^
     --fallback-extensions html --index-files "index.html,." ^
-    --root-dir docs/_site-offline docs/_site-offline %*
+    --root-dir docs/_site-offline docs/_site-offline /sep/ ^
+    --offline --no-fail --include-fragments ^
+    --root-dir docs/_site-pdf ^
+    docs/_site-pdf/book.html %*
 @popd
